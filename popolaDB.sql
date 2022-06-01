8febecc0275c085dc6ee73a531bb1a6f52fe815c
Vito Pace <99609613+vitopax@users.noreply.github.com>
2022-06-01 17:39:22 +0200

    Delete popolaDB.sql

--- popolaDB.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO CICLISTA VALUES('vit15', 'Vito', 'Pace', 'Italiana', 'S120', 1999);
-
-INSERT INTO SQUADRA VALUES('S120', 'Team', 1969, 'Torino');
-
-INSERT INTO TAPPA VALUES(20, 3, 'Torino', 'Milano', 140000, 100, 4);
-
-INSERT INTO CLASSIFICA_INDIVIDUALE(CodC,CodT,Edizione,Posizione) VALUES('vit15', 3, 20, 1);
